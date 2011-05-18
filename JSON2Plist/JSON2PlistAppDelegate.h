ed30fa8d4086c52f4610e9b9e9fe9afcbbd32c58
sodas <sodas2002@gmail.com>
2011-05-18 13:40:48 +0800

    Initial Commit

---
+++ JSON2Plist/JSON2PlistAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  JSON2PlistAppDelegate.h
+//  JSON2Plist
+//
+//  Created by sodas on 2011/5/18.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface JSON2PlistAppDelegate : NSObject <NSApplicationDelegate> {
+@private
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ JSON2Plist/JSON2PlistAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  JSON2PlistAppDelegate.m
+//  JSON2Plist
+//
+//  Created by sodas on 2011/5/18.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "JSON2PlistAppDelegate.h"
+
+@implementation JSON2PlistAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
