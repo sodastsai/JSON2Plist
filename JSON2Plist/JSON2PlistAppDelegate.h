affe15eda7ac66785f3a064f18bdaddccb4f117e
sodas <sodas2002@gmail.com>
2011-05-18 15:22:04 +0800

    Add Apache2 License

--- JSON2Plist/JSON2PlistAppDelegate.h
+++ JSON2Plist/JSON2PlistAppDelegate.h
@@ -1,11 +1,20 @@
-//
-//  JSON2PlistAppDelegate.h
-//  JSON2Plist
-//
-//  Created by sodas on 2011/5/18.
-//  Copyright 2011 NTU Mobile & HCI Research Lab. All rights reserved.
-//
-
+/*
+ * Author: sodas http://about.me/sodastsai
+ * Copyright 2011 NTU Mobile & HCI Research Lab
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
 #import <Cocoa/Cocoa.h>
 #import "JSONKit.h"
 
